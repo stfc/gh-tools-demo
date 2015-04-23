fba3484e1d679cf4d842d64e186b17f7e6a5f26a
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2015-04-23 09:57:27 +0100

    Reset for 2nd session

--- src/foo.py
+++ src/foo.py
@@ -2,9 +2,9 @@ def adder(a, b):
     return a + b
 
 
-def multiplier(a, b):
-    return a * b
+#def multiplier(a, b):
+#    return a * b
 
 
-def subber(a, b):
-    return a - b
+#def subber(a, b):
+#    return a - b
